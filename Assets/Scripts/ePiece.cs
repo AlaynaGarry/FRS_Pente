3713bf655ae492651c671547a60632eae6ba3343
Alayna Garry <agarry@student.neumont.edu>
2022-04-19 19:21:31 -0600

    4/19/22 Player class and enum

---
+++ Assets/Scripts/ePiece.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ePiece
+{
+    public enum Piece{
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        WHITE,
+        BLACK
+    }
+}
