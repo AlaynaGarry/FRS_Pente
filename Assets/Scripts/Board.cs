9dd7f61bcd22f49ceb65e2f424ae3424ecae0b37
Gus <gusmcg1856@gmail.com>
2022-04-21 18:25:25 -0600

    Created Board and Filled Out GameManager

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,5 +4,121 @@ using UnityEngine;
 
 public class Board
 {
-    
+    public ePiece[][] board = new ePiece[][]
+    {
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL },
+        new ePiece[]{ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL,ePiece.NULL }
+    };
+
+    public bool CheckWin(Player player, Vector2 placementPosition)
+    {
+        return checkDiagonalFromPosition(player, placementPosition) || checkHorizontalFromPosition(player, placementPosition) || checkVerticalFromPosition(player, placementPosition);
+    }
+
+    private bool checkVerticalFromPosition(Player player, Vector2 position)
+    {
+        int count = 0;
+        for (int i = ((int)position.y); i < position.y + 5; i++)
+        {
+            if (board[i][((int)position.x)] != player.piece)
+            {
+                break;
+            }
+            else
+            {
+                count++;
+                if (count >= 5) return true;
+            }
+        }
+        for (int i = ((int)position.y); i < position.y - 5; i--)
+        {
+            if (board[i][((int)position.x)] != player.piece)
+            {
+                break;
+            }
+            else
+            {
+                count++;
+                if (count >= 5) return true;
+            }
+        }
+        return false;
+    }
+
+    private bool checkHorizontalFromPosition(Player player, Vector2 position)
+    {
+        int count = 0;
+        for (int i = ((int)position.x); i < position.x + 5; i++)
+        {
+            if (board[((int)position.y)][i] != player.piece)
+            {
+                break;
+            }
+            else
+            {
+                count++;
+                if (count >= 5) return true;
+            }
+        }
+        for (int i = ((int)position.x); i < position.x - 5; i--)
+        {
+            if (board[((int)position.y)][i] != player.piece)
+            {
+                break;
+            }
+            else
+            {
+                count++;
+                if (count >= 5) return true;
+            }
+        }
+        return false;
+    }
+
+    private bool checkDiagonalFromPosition(Player player, Vector2 position)
+    {
+        int count = 0;
+        for (int i = ((int)position.x), j = ((int)position.y); i < position.x + 5 && j < position.y + 5; i++, j++)
+        {
+            if (board[j][i] != player.piece)
+            {
+                break;
+            }
+            else
+            {
+                count++;
+                if (count >= 5) return true;
+            }
+        }
+        for (int i = ((int)position.x), j = ((int)position.y); i < position.x - 5 && j < position.y -5; i--,j--)
+        {
+            if (board[j][i] != player.piece)
+            {
+                break;
+            }
+            else
+            {
+                count++;
+                if (count >= 5) return true;
+            }
+        }
+        return false;
+    }
 }
