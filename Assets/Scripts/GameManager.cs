95ee804659d5878120a5dfa745f9d3ac93ebd9e5
Alayna Garry <agarry@student.neumont.edu>
2022-04-21 18:31:27 -0600

    4/21/22 DisplayWin

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     List<Player> players = new List<Player>();
-
+    [SerializeField] GameObject gameWinPrefab;
 
     public void Restart()
     {
-
+    
     }
 
     public void DisplayWin()
     {
-
+        GameObject gameWin = Instantiate(gameWinPrefab, GameObject.FindWithTag("Canvas").transform);
     }
 }
